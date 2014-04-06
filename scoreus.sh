14949861a46906b051e7bc2ce8d9a4545d5d9fd8
Mermouy <publi59@gmail.com>
2014-04-06 15:09:26 +0200

    second update, ass main ui

--- scoreus.sh
+++ scoreus.sh
@@ -18,23 +18,26 @@
 # | Fifth Floor, Boston, MA  02110-1301  USA
 # +------------------------------------------------------------+
 # Simple bash + yad application to record and stat your boardgames scoring sessions
-
+mini_changelog="Nothing works for now, just playing with yad widgets...
+then will have a look at sqlite integration"
 ## Variables
 Author="MerMouY"
-Licence="GPL 3"
-Version="0.1"
+Licence="<a href=\"http://gpl3.org\">GPL 3</a>"
+Version="0.0.1"
 AppName="Scoreus"
-Install_dir=""
 # scoreus_error="$img_path/error.png"
 # scoreus_warning="$img_path/warning.png"
+Cat_list="Fast!Bit less than an hour!Between 1 and 2 Hours!Evening!All night long"
+Sub_cat_list="Cartes!Ouvriers!Management"
 scoreus_error="gtk-error"
 scoreus_warning="gtk-warning"
-scoreus_ok="gtk-ok"
+scoreus_ok="/home/mermouy/Images/scoreus/scoreus_logo.png"
 Db_path="$HOME/.scoreus"
 img_path="$Db_path/images"
 ##test variables
 DIALOG=`which yad`
-GuiName="--title=Scoreus --center --window-icon=$scoreus_ok --name=Scoreus --class=Scoreus --selectable-labels --image-path=\"$img_path\""
+
+GuiName="--title=Scoreus --center --window-icon=$scoreus_error --name=Scoreus --class=Scoreus --selectable-labels --image-path=\"$img_path\""
 
 
 ### Functions
@@ -59,7 +62,7 @@ else
 fi
 }
 
-function add_player()
+function add_player_ui()
 {
 player_data=($($DIALOG $GuiName --form --text="<span size=\"x-large\"><b>Add a new player to the database: </b></span>" \
 	--always-print-result --separator=" " \
@@ -90,14 +93,123 @@ player_data=($($DIALOG $GuiName --form --text="<span size=\"x-large\"><b>Add a n
 		elif [ "$player_data[5]" = "false" ] && [ "$player_data[6]" = "true" ]
 			then p_powers="Players admin status"
 		fi
-		player_data_record=$($DIALOG $GuiName --text="<b>Verify entered data to avoid any mistake.</b>\n\nIf you agree with these data, click <i>Inser</i>' button.\n\n\n<i>Player avatar should be displayed on the left.</i>\n\n<b>Player Name:</b> <span color=\"blue\">$p_name</span>\n\n<b>Player Website: </b><span color=\"blue\">$p_http</span>\n\n<b>Player email address: </b><span color=\"blue\">$p_email</span>\n\n<b>$p_name will have $p_powers</b>\n\n<b>And finally some part of his life: </b><span color=\"blue\">\"$p_bio\"</span>" --button="Insert":0 --button=Cancel:1 --image="$p_avatar")
+		player_data_record=$($DIALOG $GuiName --text="<b>Verify entered data to avoid any mistake.</b>\n\nIf you agree with these data, click \"<i>Insert</i>\" button.\n\n\n<i>Player avatar should be displayed on the left.</i>\n\n<small>	Or there was an error with upload/selection.</small>\n\n<b>Player Name:</b> <span color=\"blue\">$p_name</span>\n\n<b>Player Website: </b><span color=\"blue\">$p_http</span>\n\n<b>Player email address: </b><span color=\"blue\">$p_email</span>\n\n<b>$p_name will have $p_powers</b>\n\n<b>And finally some part of his life: </b><span color=\"blue\">\"$p_bio\"</span>" --button="Insert":0 --button=Cancel:1 --image="$p_avatar")
 		if [ "$?" = "0" ]
 			then
-			echo "on y va\!"
+			echo "on y va"
 		else exit 1
 		fi
 	else exit 1
 	fi
+}
+
+function add_play_ui()
+{
+echo "$mini_changelog" | $DIALOG $GuiName --title="Scoreus Add Play" --text="<big><b>Coming soon...</b></big>\n\nFor now just the mini changelog: \n" --text-info --button=Quit:1 --width=500
+}
+
+function add_game_ui()
+{
+g_data=($($DIALOG $GuiName --title="Scoreus Add Game" --text="<span size=\"x-large\"><b>Add a new gmae to the database: </b></span>" \
+--form --always-print-result --separator=" " \
+--field="Game Name: " "Game Name" \
+--field="Game category: ":cb "$Cat_list" \
+--field="Game subcategory: ":cb "$Sub_cat_list" \
+--field="Website: " http://gamesite.com \
+--field="Author(s): " "A famous one?" \
+--field="Editor: " "Ravensburger?" \
+--field="Owner(s): " "mermouy" \
+--field="Game Image: ":sfl "$HOME" \
+--field="If your image is not already on this computer\nCheck this to upload it now.":chk FALSE \
+--field="Score Board: ":chk "true" \
+--field="Game Synopsis: ":txt \
+--colums=2
+))
+
+if [ "$?" = "0" ]
+	then
+	g_name=${g_data[0]}
+	g_cat=${g_data[1]}
+	g_website=${g_data[3]}
+	g_http="<a href=\"$g_website\">Website of \"$g_name\"</a>"
+	g_author=${g_data[4]}
+	g_editor=${g_data[5]}
+	g_owner=${g_data[6]}
+	g_img=${g_data[7]}
+	g_synopsis=${g_data[10]}
+	if [ "${g_data[9]}" = "true" ]
+		then
+		add_score_board
+	fi
+	if [ "${g_data[8]}" = "true" ]
+		then
+		echo "upload file"
+	fi
+	game_data_record=$($DIALOG $GuiName --text="<b>Verify entered data to avoid any mistake.</b>\n\n \
+	If you agree with these data, click \"<i>Insert</i>\" button.\n\n\n \
+	<i>Game image should be displayed on the left.</i>\n\n \
+	<small>	Or there was an error with upload/selection.</small>\n\n \
+	<b>Game Name: </b> <span color=\"blue\">\"${g_data[0]}\"</span>\n\n \
+	<b>Game Categorie: </b><span color=\"blue\">${g_data[1]}</span>\n\n \
+	<b>Game Website: </b><span color=\"blue\">"$g_http"</span>\n\n \
+	<b>Game Author: </b><span color=\"blue\">\"${g_data[4]}\"</span>\n\n \
+	<b>Game Editor: </b><span color=\"blue\">\"${g_data[0]}\"</span>\n\n \
+	<b>Game Owner(s) (separated with colon): </b><span color=\"blue\">\"${g_data[6]}\"</span>\n\n \
+	<b>And finally a little summary: </b><span color=\"blue\">\"${g_data[10]}\"</span>" \
+	--button="Insert":0 --button=Cancel:1 --image=${g_data[7]})
+	if [ "$?" = "0" ]
+		then
+		echo "on y va"
+	else exit 1
+	fi
+else exit 1
+fi
+}
+
+function config_ui()
+{
+$DIALOG $GuiName --title="Scoreus Config" --text="<big><b>Coming soon...</b></big>\n" --button=Quit:1
+}
+
+function help_ui()
+{
+$DIALOG $GuiName --title="Scoreus help" --text="What? You really need help for that shit not working?\n\nPlease...\n\n$help" --button=Quit:1
+}
+
+function about_ui()
+{
+$DIALOG $GuiName --title="About Scoreus" --text="$AppName Version $Version is slowly and poorly developped by $Author\n \
+This is a simple bash script with easy widgets via yad or zenity to record your boardgames plays scores\n\n \
+<span color=\"green\">$mini_changelog</span>\n\n \
+The project is on github and should take long to upgrade as I'm not a real developper...\n \
+If you are developper, I will be happy to learn advices from you but,\n \
+Please do not give all answers without child-level explanations!\n \
+If you're a beginner as I am feel free to participate to this small project.\n \
+Please contact me if you need help like \"where to start\"\n \
+Github repo: <a href=\"https://github.com/Mermouy/scoreus\">https://github.com/Mermouy/scoreus</a>\n" \
+--button=Quit:1 --width=400 --height=280 --image="$scoreus_ok" --image-on-top
+}
+
+function welcome_ui() {
+$DIALOG $GuiName --form --text="<span size=\"xx-large\"><b><u>Welcome in Scoreus</u></b></span>\n\nChoose what you want to do: \n" \
+--width=300 --height=200 --always-print-result --image="$scoreus_ok" \
+--field="Add Play!gtk-ok":fbtn  "./$0 --addplay" \
+--field="Add Player!gtk-ok":fbtn "./$0 --addplayer" \
+--field="Add Game!gtk-ok":fbtn "./$0 --addgame" \
+--field="Config!gtk-ok":fbtn "./$0 --config" \
+--field="Statistics!gtk-ok":fbtn "./$0 --stats" \
+--field="Help!gtk-help":fbtn "./$0 --help" \
+--field="About!gtk-help":fbtn "./$0 --about" \
+--button=Quit:1 --buttons-layout=spread --image-on-top
+if [ "$?" = "1" ]
+	then
+	exit 1
+fi
+}
+
+function stats_ui()
+{
+$DIALOG $GuiName --title="Scoreus help" --text="What? You really think stats are available?\n\nPlease...\n\nSeriously...\n\n$help" --button=Quit:1
 }
 
 # Look for recorded options while installed
@@ -121,6 +233,31 @@ player_data=($($DIALOG $GuiName --form --text="<span size=\"x-large\"><b>Add a n
 # 	fi
 # 	;;
 # esac
-add_player
-echo $player_data | sed -e 's/|//g'
+#add_player
 #welcome_form
+case $1 in
+	"")
+		welcome_ui
+		;;
+	"--config")
+		config_ui
+		;;
+	"--addplayer")
+		add_player_ui
+		;;
+	"--addgame")
+		add_game_ui
+		;;
+	"--addplay")
+		add_play_ui
+		;;
+	"--stats")
+		stats_ui
+		;;
+	"--about")
+		about_ui
+		;;
+	"--help")
+		help_ui
+		;;
+esac
